4e7f7ac2a4c16d0658aa331459b72f1c9c1a550b
stasrostov <promotop7@gmail.com>
2024-07-26 12:05:45 +0300

    init

---
+++ src/components/ShopItemClass/ShopItemClass.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './shopItemClass.css'
+
+class ShopItemClass extends Component {
+      render(){
+        const {brand, title, description, descriptionFull, price, currency} = this.props.info
+        return (
+            <div className ="main-content">
+               <h2>{brand}</h2>
+                <h1>{title}</h1>
+                <h3>{description}</h3>
+            <div className ="description">
+                {descriptionFull}
+            </div>
+  <div className ="highlight-window mobile"><div className ="highlight-overlay"></div></div>
+  <div className ="divider"></div>
+  <div className ="purchase-info">
+    <div className ="price">{currency}{price}</div>
+    <button>Добавить в корзину</button>
+  </div>
+</div>
+               
+        )
+      }
+}
+
+export default ShopItemClass
+ 
